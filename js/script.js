982f4dc36296730de7750fce406faab4979a4729
Tayrlock <dkfear@ya.ru>
2022-02-10 08:56:13 +0300

    dynamic day menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// 'use strick'
 window.addEventListener('DOMContentLoaded', () => {
     //Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
@@ -125,17 +126,57 @@ window.addEventListener('DOMContentLoaded', () => {
     })
 
     // add showTimer to Modal
-    const modalTimerId = setTimeout(showModal, 5000)
+    // const modalTimerId = setTimeout(showModal, 5000)
 
-    function showModalByScroll(){
-        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-            showModal()
-            window.removeEventListener('scroll', showModalByScroll)
+    // function showModalByScroll(){
+    //     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+    //         showModal()
+    //         window.removeEventListener('scroll', showModalByScroll)
+    //     }
+    // }
+
+    // window.addEventListener('scroll', showModalByScroll)
+
+
+    // confige Menu
+
+    class Menu {
+        constructor(img, alt, subtitle, descr, price) {
+            this.img = img
+            this.alt = alt
+            this.subtitle = subtitle
+            this.descr = descr
+            this.price = price
+            this.img = img
+            this.curse = '1.5'
+            this.switchToUAH()
+            this.render()
         }
+        switchToUAH() {
+            this.price = Math.floor(+this.price * +this.curse)
+        }
+        render() {
+            const div = document.createElement('div')
+            div.innerHTML = `
+            <div class="menu__item">
+                    <img src=${this.img} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.subtitle} </h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>`
+
+            document.querySelector('.menu__field .container').append(div)
+            console.log(div)
+
+        }
+
     }
 
-    window.addEventListener('scroll', showModalByScroll)
+    const menuFitnes = new Menu("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', '229'),
+        menuPremium = new Menu('img/tabs/elite.jpg', 'elite', 'Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', '550'),
+        menuPost = new Menu('img/tabs/post.jpg', 'post', 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', '430')
 
-    console.log(modalTrigger)
-    console.log(modal)
 })
